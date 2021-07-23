4f40f735834317b5f595efb0f9a3f41a0278d068
Gabriel <gcast200414@gmail.com>
2021-07-23 16:54:40 -0400

    se borro boostrap, se agregaron animaciones css, tabla y color de fondo

--- Vistas/actores.html.php
+++ Vistas/actores.html.php
@@ -1,6 +1,41 @@
 <?php require_once 'partes/head.php';?>
 <?php require_once 'partes/menu.php';?>
 
+
+
+
+<!--Hey! This is the original version
+of Simple CSS Waves-->
+
+<div class="header">
+
+
+
+<!--Waves Container-->
+<div>
+<svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+<defs>
+<path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+</defs>
+<g class="parallax">
+<use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+<use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+<use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+<use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+</g>
+</svg>
+</div>
+<!--Waves end-->
+
+</div>
+
+<!--Content ends-->
+
+
+
+
+
 <div class="bg-light">
     <div class="container">
         <h1 class="display-1 py-5"><?php echo $pagina; ?></h1>
@@ -22,27 +57,39 @@
 </div>
 
 <!--Tabla-->
-<!-- Cabecera de la tabla 🡻 -->
+<!-- Cabecera de la tabla 🡻 
 <div class= "table container">
     <table class="table table-dark table-striped">
     <thead>
         <tr class="table-primary">
         <th scope="col">#</th>
         <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">Last</th>-->
+
 
 <!-- Tabla y contenido 🡻 -->
-<table class="table table-dark table-striped">
-    <thead>
-        </tr class="table-primary">
-    </thead>
-    <tbody>
 
 
-
-
-        <?php
+<section>
+  <!--for demo wrap-->
+  <h1>Fixed Table header</h1>
+  <div class="tbl-header">
+    <table cellpadding="0" cellspacing="0" border="0">
+      <thead>
+        <tr>
+          <th>Code</th>
+          <th>Company</th>
+          <th>Price</th>
+          <th>Change</th>
+          <th>Change %</th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+  <div class="tbl-content">
+    <table cellpadding="0" cellspacing="0" border="0">
+      <tbody>
+      <?php
         while ($dato = mysqli_fetch_assoc($resultado)) {
             echo "<tr>
                     <th  scope='row'>{$dato['actor_id']}</th>
@@ -52,8 +99,19 @@
                 </tr>";
         }
         ?>
-    </tbody>
-</table>
+      </tbody>
+    </table>
+  </div>
+</section>
+
+
+
 </div>
+
+</div>
+
+
+
+
 
     <?php require_once 'partes/foot.php'; ?>
